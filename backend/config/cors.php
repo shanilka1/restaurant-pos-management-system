7037c1b3af0a7973b9f2b1188fd0cefe43f6b36f
Kawya Dissanayaka <shanilkamax111@gmail.com>
2026-09-16 09:41:33 +0530

    push

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -12,7 +12,15 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => [
+        'http://localhost:3000',
+        'http://localhost:5173',
+        'http://127.0.0.1:3000',
+        'http://127.0.0.1:5173',
+        'https://minipos-app-123.loca.lt',
+        'https://frontend-iota-three-56.vercel.app',
+        'https://frontend-jm21jdly3-kawyadissanayakas-projects.vercel.app'
+    ],
 
     'allowed_origins_patterns' => [],
 
